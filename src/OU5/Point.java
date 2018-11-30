8811ef47564b338823a6c29bd661ad847c387705
Oxbrew <13368707+Oxbrew@users.noreply.github.com>
2018-11-30 11:47:09 +0100

    Continuing OU5

--- src/OU5/Point.java
+++ src/OU5/Point.java
@@ -8,13 +8,13 @@ public class Point {
     public Point(){   //default constructor
     }
 
-    public  Point(String id, int x, int y){
+    public  Point(String name, int x, int y){  //explicit constructor
         this.name = name;
         this.x = x;
         this.y = y;
     }
 
-    public Point(Point p) {
+    public Point(Point p) { // copy constructor where 'p' is point being copied
         name = p.name;
         x = p.x;
         y = p.y;
@@ -22,7 +22,7 @@ public class Point {
 
     public String getName() {
         return name;
-    }
+    } // inspector for Name
 
     public  int getX() {
         return x;
@@ -30,6 +30,18 @@ public class Point {
 
     public  int getY() {
         return y;
+    } // inspector for Y
+
+
+    public void setX(int x) { //mutator for X
+        this.x = x;
     }
 
+    public void setY(int y) { //mutator for Y
+        this.y = y;
+    }
+
+    public  double distance(Point p){ //distance method
+        return Math.sqrt(Math.pow((x - p.x),2) + Math.pow((y- p.y),2));
+    }
 }
--- src/OU5/PointTest.java
+++ src/OU5/PointTest.java
@@ -1,7 +1,36 @@
 package OU5;
 
+import java.io.*; //importing for PrintWriter
+
 public class PointTest {
     public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
 
+        // Testing a constructor and a transformer
+        Point p1 = new Point("A", 3, 4);
+        Point p2 = new Point("B", 5, 6);
+        out.println(p1 + "  " + p2);
+
+        // Testing inspectors
+        String n = p1.getName();
+        int x = p1.getX();
+        int y = p1.getY();
+        out.println(n + " " + x + " " + y);
+
+        //Testing a combiner and a comparator
+        double d = p1.distance(p2);
+        out.println(d);
+        boolean b = p1.equals(p2);
+        out.println(b);
+
+        //Testing mutators
+        p2.setX(1);
+        p2.setY(2);
+        out.println(p2);
+
+
+        //Testing another constructor
+        Point p = new Point(p1);
+        out.println(p);
     }
 }
