925e12049354d67b8e4feec1e5d83d62274b73ff
Oxbrew <{13368707}+{Oxbrew}@users.noreply.github.com>
2018-12-14 09:35:34 +0100

    eu1

--- src/EU1/Eu1.java
+++ src/EU1/Eu1.java
@@ -12,7 +12,7 @@ public static void main(String[] args) {
     for (int i = 0; i < elements.length; i++){
         elements[i] = in.nextInt(); 
     }
-    System.out.println("The least element is: " + minimum(elements));
+    System.out.println("The least element is: " + min(elements));
 
 }
 
@@ -21,47 +21,46 @@ public static void main(String[] args) {
 // collection . If the collection is empty an
 // IllegalArgumentException is thrown .
 public static int min ( int [] elements ) throws IllegalArgumentException {
-if ( elements . length == 0)
-throw new IllegalArgumentException (" empty collection ");
+    if ( elements . length == 0)
+        throw new IllegalArgumentException (" empty collection ");
 // Is used in trace printing 2:
-int nofIters = 1;
-int [] sequence = elements ;
-int nofPairs = sequence . length / 2;
-int nofUnpairedElements = sequence . length % 2;
+    int nofIters = 1;
+    int [] sequence = elements ;
+    int nofPairs = sequence . length / 2;
+    int nofUnpairedElements = sequence . length % 2;
+    int nofPossibleElements = nofPairs + nofUnpairedElements ;
+    int [] partialSeq = new int [ nofPossibleElements ];
+    int i = 0;
+    int j = 0;
 
-int nofPossibleElements = nofPairs + nofUnpairedElements ;
-int [] partialSeq = new int [ nofPossibleElements ];
-int i = 0;
-int j = 0;
-while ( nofPairs *2 + nofUnpairedElements > 1) // this was previously sequence.length
-{
+    while ( nofPairs >= 1) /*(sequence.length > 1)*/ {
 // extract a partial sequence of possible elements
-i = 0;
-j = 0;
-while (j < nofPairs )
-{
-partialSeq [j++] = ( sequence [i] < sequence [i + 1]) ?
-sequence [i] : sequence [i + 1];
-i += 2;
-}
-if ( nofUnpairedElements == 1)
-partialSeq [j-1] = sequence [ (nofPairs*2 + nofUnpairedElements) - 1]; // This used to also be sequence.length
-// now turn to the partial sequence
-sequence = partialSeq ;
-nofPairs = nofPossibleElements / 2;
-nofUnpairedElements = nofPossibleElements % 2;
-nofPossibleElements = nofPairs + nofUnpairedElements ;
-// Trace printing 1 - to follow the sequence
-System . out. println ( java . util . Arrays . toString ( sequence ));
-// Trace printing 2 - to terminate the loop preemptively
-// (to be able to see what happens initially )
-if ( nofIters ++ == 10)
-System . exit (0);
-}
+        i = 0;
+        j = 0;
+        while (j < nofPairs )
+        {
+        partialSeq [j++] = ( sequence [i] < sequence [i + 1]) ?
+        sequence [i] : sequence [i + 1];
+        i += 2;
+        }
+        if ( nofUnpairedElements == 1)
+        partialSeq [j] = sequence[nofPairs * 2]; // This used to also be sequence.length - 1
+    // now turn to the partial sequence
+        sequence = partialSeq ;
+        nofPairs = nofPossibleElements / 2;
+        nofUnpairedElements = nofPossibleElements % 2;
+        nofPossibleElements = nofPairs + nofUnpairedElements ;
+        // Trace printing 1 - to follow the sequence
+        System . out. println ( java . util . Arrays . toString ( sequence ));
+        // Trace printing 2 - to terminate the loop preemptively
+        // (to be able to see what happens initially )
+        if ( nofIters ++ == 10)
+        System . exit (0);
+        }
 // sequence [0] is the only remaining possible element
 // - it is the least element
-return sequence [0];
-}
+    return sequence [0];
+    }
 
     private static int minimum (int [] elements) {
         int smallest = elements[0];
