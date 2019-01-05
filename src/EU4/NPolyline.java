7b0b99ee12227a0fd1602824fbde5cb842b12367
Oxbrew <{13368707}+{Oxbrew}@users.noreply.github.com>
2019-01-05 19:39:53 +0100

    Continuation

--- src/EU4/NPolyline.java
+++ src/EU4/NPolyline.java
@@ -1,5 +1,7 @@
 package EU4;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class NPolyline implements Polyline {
     private static class Node {
         public Point vertex ;
@@ -63,11 +65,77 @@ public class NPolyline implements Polyline {
     }
     public double length() {
         double len = 0;
-        Node n = this.vertices;
+        Node n = this.vertices; // vertices.nextNode?
         Point p = new Point(this.vertices.vertex);
         while (n != null){
-            len += n.vertex.distance(p)
+            len += Math.sqrt(Math.pow(p.getX() - n.vertex.getX(),2) + Math.pow(p.getY()-n.vertex.getY(),2)); // can we use .distance?
+            p = n.vertex;
+            n = n.nextNode;
         }
+        return len;
+    }
+    public void add(Point vertex){
+        Node node = new Node(vertex);
+        if (this.vertices == null) {
+            this.vertices = node;
+        }
+        Node q = this.vertices;
+        while (q.nextNode != null){
+            q = q.nextNode;
+        }
+        q.nextNode = node;
+        size++;
+    }
+    public void insertBefore(Point vertex, String vertexName)throws NoSuchElementException {
+    Node node = new Node(vertex);
+    if (this.vertices.vertex.getName().equals(vertexName)){
+        node.nextNode = this.vertices;
+        this.vertices = node;
+    }
+    Node n = this.vertices;
+    boolean has = false;
+    while (n.nextNode != null){
+        if (n.nextNode.vertex.getName().equals(vertexName)){
+            node.nextNode = n.nextNode;
+            n.nextNode = n;
+            has = true;
+            n = n.nextNode;
+        }
+        n = n.nextNode;
+    }
+    size++;
+    if (!has){
+        throw new NoSuchElementException("None found");
+    }
+    }
+    public void remove(String vertexName)throws NoSuchElementException {
+        Node n = this.vertices;
+        boolean has = false;
+        if (this.vertices.vertex.getName().equals(vertexName)){
+            this.vertices = this.vertices.nextNode;
+        }
+        while (n != null){
+            if (n.vertex.getName().equals(vertexName)){
+                n = n.nextNode;
+                has = true;
+                size--;
+            }
+            n = n.nextNode;
+        }
+        if(!has){
+            throw new NoSuchElementException("None found");
+        }
+    }
+    public Iterator<Point> iterator(){
+        return new Iterator<Point>() {
+            Node node = vertices;
+            public boolean hasNext() {
+                return false;
+            }
 
+            public Point next() {
+                return null;
+            }
+        }
     }
 }
